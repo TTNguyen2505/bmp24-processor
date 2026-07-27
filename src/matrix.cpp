e46dd59ab724a971b6779906e10d2a1b42c8fba6
unknown <datrubik3x3@gmail.com>
2026-07-27 18:54:40 +0700

    implement multiplication for 4x4 matrices

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -16,8 +16,17 @@ Matrix3x3 operator*(const Matrix3x3 &lhs, const Matrix3x3 &rhs) {
 }
 
 Matrix4x4 operator*(const Matrix4x4 &lhs, const Matrix4x4 &rhs) {
-    // TODO: Multiply two 4x4 matrices using row-by-column multiplication.
-    return {};
+    // Multiply two 4x4 matrices: result[i][j] = sum(lhs[i][k] * rhs[k][j])
+    Matrix4x4 result;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result.data[i][j] = 0.0;
+            for (int k = 0; k < 4; ++k) {
+                result.data[i][j] += lhs.data[i][k] * rhs.data[k][j];
+            }
+        }
+    }
+    return result;
 }
 
 Matrix3x3 createTranslationMatrix(double tx, double ty) {
